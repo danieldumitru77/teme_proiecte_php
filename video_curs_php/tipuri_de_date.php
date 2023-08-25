0e0aa839c639dace0591e15d3697ab7d6f79dfda
danieldumitru77 <danieldumitru77@yahoo.com>
2023-08-25 15:17:11 +0200

    tipuri de date

---
+++ video_curs_php/tipuri_de_date.php
@@ -0,0 +1,33 @@
+<?php
+//Scalar type 
+
+
+ //Integer
+ $a=1234;
+ $b=-123;
+ $c=0123;
+ $d=0x1A;
+ $e=0b11111111;
+ 
+ //float
+ $f=1.234;
+ $g=1.2e3;
+ $h=7E-10;
+ 
+ //string
+ $i='thisis a string';
+ $j="this is a string";
+ $k="100";
+ $l='this is a 
+ simple string';
+ 
+ // Boolean - nu e case sensitive
+ 
+ $m=True;
+ $n=TRUE;
+ $o=true;
+ $p=false;
+ 
+ 
+ 
+?>
